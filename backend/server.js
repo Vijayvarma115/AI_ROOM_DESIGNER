adb6407a50b969a9e11aa9a6d91ddde234c37639
Vijay Varma <145138789+Vijayvarma115@users.noreply.github.com>
2025-07-23 10:28:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const connectDB = require('./config/database');
 const { DesignHistory, Feedback, ApiUsage } = require('./models');
 
-// Load environment variables
+// Load environment variables as per
 dotenv.config();
 
 // Connect to MongoDB
